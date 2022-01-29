f6bf5987772d968821446151ec1fe563d231b8a1
Sebastian Sigl <akrillo89@gmail.com>
2022-01-29 14:45:53 +0100

    first commit

---
+++ src/main/kotlin/org/fullsack101/bandit/Hello.kt
@@ -0,0 +1,6 @@
+package org.fullsack101.bandit
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/org/fullsack101/bandit/HelloTest.kt
@@ -0,0 +1,8 @@
+package org.fullsack101.bandit
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
