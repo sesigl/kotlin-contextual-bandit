2a6d77c98b46407d78a85ee30c5f0442f20a4aee
Sebastian Sigl <akrillo89@gmail.com>
2022-02-21 18:37:36 +0100

    Add missing code

--- src/main/kotlin/org/fullsack101/bandit/context/ContextAttribute.kt
+++ src/main/kotlin/org/fullsack101/bandit/context/ContextAttribute.kt
@@ -1,6 +1,16 @@
 package org.fullsack101.bandit.context
 
+import com.fasterxml.jackson.annotation.JsonCreator
+
 /**
  * Defines a unique context attribute for a context.
  */
-data class ContextAttribute(val key: ContextAttributeKey, val value: ContextAttributeValue)
+
+data class ContextAttribute(val key: ContextAttributeKey, val value: ContextAttributeValue) {
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun create(key: String, value: String) =
+            ContextAttribute(ContextAttributeKey(key), ContextAttributeValue(value))
+    }
+}
